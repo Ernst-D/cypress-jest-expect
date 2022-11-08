2757ae10234e60ced320f7d26b203c827c9a1b4c
Ernst-D <ernst.dzerevanko@gmail.com>
2022-11-08 20:11:45 +0400

    fixed config, disabled videos and screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
+const { expect } = require("expect")
 
 module.exports = defineConfig({
   e2e: {
+    // ernst: videos and screenshots
+    video: false,
+    screenshotOnRunFailure:false,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("task",{
+          _expect({actual, expected}){
+            console.debug(`actual: ${actual}`)
+            console.debug(`expected: ${expected}`)
+            expect(actual).toEqual(expected)
+
+            return null;
+          }
+      })
     },
   },
 });
